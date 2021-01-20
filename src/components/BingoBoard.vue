dd875f6162d037f35d9a3cf62a9ed7d98682ed70
Alejandro Rodríguez <rs.alejandro.93@gmail.com>
2021-01-20 19:23:07 +0100

    Basic radomizing of cards

--- src/components/BingoBoard.vue
+++ src/components/BingoBoard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <BoardCell keyword="keyword" ship="ship" au="au" />
+    <BoardCell :keyword="keyword" :ship="ship" :au="au" />
   </div>
 </template>
 
@@ -8,7 +8,41 @@
 import BoardCell from './BoardCell'
 
 export default {
-  components: {BoardCell}
+  components: {BoardCell},
+  data () {
+    return {
+      au: '',
+      ship: '',
+      keyword: ''
+    }
+  },
+  methods: {
+    getRandomNumber (max) {
+      return Math.floor(Math.random() * max)
+    },
+    async getAU () {
+      const response = await (await fetch('../data/auSettings.json')).json()
+
+      this.au = response[this.getRandomNumber(response.length)]
+    },
+    async getShip () {
+      const response = await (await fetch('../data/ships.json')).json()
+
+      this.ship = response[this.getRandomNumber(response.length)]
+    },
+    async getKeyword () {
+      const nsfw = this.getRandomNumber(2)
+      const result = nsfw ? (await fetch('../data/nsfwKeywords.json')) : (await fetch('../data/sfwKeywords.json'))
+      const keywords = await result.json()
+
+      this.keyword = keywords[this.getRandomNumber(keywords.length)]
+    }
+  },
+  mounted () {
+    this.getAU()
+    this.getShip()
+    this.getKeyword()
+  }
 }
 </script>
 
